bbd3202bde8972a7f11f7718b5b0704d2d662393
pellburt@gmail.com <pellburt@gmail.com>
2019-02-05 07:06:55 +0100

    Removed background image

---
+++ js/logic.js
@@ -0,0 +1,15 @@
+let i = 0;
+let speed = 50;
+let element = document.querySelector(".typing");
+let text = element.innerHTML;
+element.innerHTML = "";
+
+function typeWriter() {
+  if (i < text.length) {
+    element.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter();
