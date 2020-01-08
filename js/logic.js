f4fbc3d083c063a84148ac5ebe21719ccf5864ac
evilpaper <pellburt@gmail.com>
2020-01-08 19:28:00 +0100

    Wrap JavaScript in an IIFE

--- js/logic.js
+++ js/logic.js
@@ -1,15 +1,17 @@
-let i = 0;
-let speed = 50;
-let element = document.querySelector(".typing");
-let text = element.innerHTML;
-element.innerHTML = "";
+(function() {
+  let i = 0;
+  let speed = 50;
+  let element = document.querySelector(".typing");
+  let text = element.innerHTML;
+  element.innerHTML = "";
 
-function typeWriter() {
-  if (i < text.length) {
-    element.innerHTML += text.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+  function typeWriter() {
+    if (i < text.length) {
+      element.innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
   }
-}
 
-typeWriter();
+  typeWriter();
+})();
